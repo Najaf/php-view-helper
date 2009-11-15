381636673f6ad328d7bd1d2cb2a2ab8f3c5ff1f9
Najaf Ali <ali.najaf@gmail.com>
2009-11-15 14:11:17 +0000

    added unit tests

---
+++ viewhelpertest.php
@@ -0,0 +1,8 @@
+<?php
+require 'PHPUnit/Framework/TestCase.php';
+  class ViewHelperTest extends PHPUnit_Framework_TestCase {
+    function test_true() {
+      $this->assertTrue(true);
+    }
+  }
+?>
